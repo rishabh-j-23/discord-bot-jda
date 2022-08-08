afeffebd7bd4fc0f8be5a5509534dfd5e0b387b2
Ryxke <99457863+Ryxke@users.noreply.github.com>
2022-08-08 15:33:49 +0000

    initial commit

---
+++ yelan/src/main/java/com/ryxke/yelan/Main.java
@@ -0,0 +1,10 @@
+package com.ryxke.yelan;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main{
+    public static void main(String[] args) {
+                
+    }
+}
